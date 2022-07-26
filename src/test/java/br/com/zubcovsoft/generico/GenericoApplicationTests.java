8a90940c595a4707320e52300598e624506b88c6
Alisson Zubcov <alissonzubcov@gmail.com>
2022-07-25 23:29:32 -0300

    first commit

---
+++ src/test/java/br/com/zubcovsoft/generico/GenericoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zubcovsoft.generico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
